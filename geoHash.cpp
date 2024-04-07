c52fd7b041e396483e9f997b810f7616db0bff1e
sddhnt4693 <siddhant.sinha4693@gmail.com>
2024-04-07 14:23:27 +0530

    haversine formula

--- geoHash.cpp
+++ geoHash.cpp
@@ -18,8 +18,25 @@ class GeoHash {
                          vector<Customer> customerList) {
 
     }
-    long double calculateHaversineDist (pair<int, int>& source,
-                                        pair<int, int>& dest) {
 
+    double calculateHaversineDist (pair<double, double>& source,
+                                        pair<double, double>& dest) {
+        double lat1 = source.first, lon1 = source.second;
+        double lat2 = dest.first, lon2 = dest.second;
+
+        double dLat = (lat2 - lat1) *
+                      M_PI / 180.0;
+        double dLon = (lon2 - lon1) *
+                      M_PI / 180.0;
+
+        lat1 = (lat1) * M_PI / 180.0;
+        lat2 = (lat2) * M_PI / 180.0;
+
+        double a = pow(sin(dLat / 2), 2) +
+                   pow(sin(dLon / 2), 2) *
+                   cos(lat1) * cos(lat2);
+        double rad = 6371;
+        double c = 2 * asin(sqrt(a));
+        return rad * c;
     }
 };
